21d4124b8b18842b21a736e068370da563e2aa5e
krishna <krishnavijayan34@gmail.com>
2026-04-20 11:17:17 +0530

    Assignment2

---
+++ src/welcome.jsx
@@ -0,0 +1,11 @@
+import './welcome.css';
+function Welcome() {
+  return (
+    <div className="container">
+      <h1>Welcome to My Page</h1>
+      <p>This is my first paragraph about react component using external css</p>
+    </div>
+  );
+}
+
+export default Welcome;
