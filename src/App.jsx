a182bbd27c24ca0520d8b2098963869a6b56cd95
krishna <krishnavijayan34@gmail.com>
2026-04-21 10:54:17 +0530

    Login and Logout

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,23 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
-//import MyComponent from "./newcomponent";
-//import MyName from "./myname";
-//import Welcome from "./welcome.jsx"
-//import ProductCard from './productcard.jsx';
-//import UserInfo from './userinfo.jsx';
-//import { useState } from "react";
-import Child from "./child.jsx";
+
 
 function App() {
-const [rating, setRating] = useState(0);
-
-  function handleRating(value) {
-    setRating(value);
-  }
+const [isLoggedIn, setIsLoggedIn] = useState(false);
+ const handleLoginLogout = () => {
+    setIsLoggedIn(!isLoggedIn);
+  };
 
   return (
     <div>
-      <h1>Rating Component</h1>
+      <h1>{isLoggedIn ? "Welcome User" : "Please Login"}</h1>
 
-      <h2>{"⭐".repeat(rating)}</h2>
-
-      <Child sendRating={handleRating} />
+      <button onClick={handleLoginLogout}>
+        {isLoggedIn ? "Logout" : "Login"}
+      </button>
     </div>
   );
 }
 
 export default App;
-
