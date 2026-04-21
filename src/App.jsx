4650cb63d756a34030345c5334bf2393b36bf951
krishna <krishnavijayan34@gmail.com>
2026-04-21 11:23:19 +0530

    login and logout

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-
-
+import LoginToggle from "./Login.jsx";
 
 function App() {
-const [isLoggedIn, setIsLoggedIn] = useState(false);
- const handleLoginLogout = () => {
-    setIsLoggedIn(!isLoggedIn);
-  };
-
-  return (
+ return (
     <div>
-      <h1>{isLoggedIn ? "Welcome User" : "Please Login"}</h1>
-
-      <button onClick={handleLoginLogout}>
-        {isLoggedIn ? "Logout" : "Login"}
-      </button>
+    <LoginToggle />  
     </div>
   );
 }
