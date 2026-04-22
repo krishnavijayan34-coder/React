b2b22dc42af1355146fe17b66ea35643955255bc
Krishna Vijayan <krishnavijayan34@gmail.com>
2026-04-22 23:14:52 +0530

    modified

---
+++ App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+//import LoginToggle from "./Login.jsx";
+//import UserStatus from "./userstatus.jsx";
+// import AdminPanel from "./AdminPanel.jsx";
+// import StudentPanel from "./StudentPanel.jsx";
+// import TeacherPanel from "./TeacherPanel.jsx";
+// import FruitList from "./FruitList";
+// import StudentList from "./StudentList";
+
+// import LoginForm from "./Loginformobject.jsx";
+// import ThemeToggle from "./ThemeToggle";
+
+// import BackgroundColorChanger from "./BackgroundColorChanger"
+// import WindowWidth from "./WindowWidth";
+// import CountdownTimer from "./CountdownTimer.jsx";
+// import UserList from "./UserList";
+import ThemeContext from "./ThemeContext";
+import Home from "./Home";
+ 
+function App() {
+  return (
+    <ThemeContext.Provider value="dark">
+      <Home />
+    </ThemeContext.Provider>
+  );
+}
+
+export default App;
