28693dec3011d820aa7ba30c44e9d484ed599e6a
Krishna Vijayan <krishnavijayan34@gmail.com>
2026-04-23 17:44:00 +0530

    modified

--- App.jsx
+++ App.jsx
@@ -4,31 +4,18 @@ import viteLogo from './assets/vite.svg'
 //import heroImg from './assets/hero.png'
 import './App.css'
 
-import EmployeeContext from "./EmployeeContext.jsx";
-import Dashboard from "./Dashboard.jsx";
- 
+// import EmployeeContext from "./EmployeeContext.jsx";
+// import Dashboard from "./Dashboard.jsx";
+//import CounterRef from "./CounterRef";
+//import LeaveApply from "./LeaveApply";
+import ToggleComponent from "./ToggleComponent";
 function App() {
-  const [employee, setEmployee] = useState({
-    name: "Krishna",
-    role: "Developer",
-    leaveBalance: 10,
-  });
-
-  const applyLeave = (leaveTaken) => {
-    if (leaveTaken > 0 && leaveTaken <= employee.leaveBalance) {
-      setEmployee({
-        ...employee,
-        leaveBalance: employee.leaveBalance - leaveTaken,
-      });
-    } else {
-      alert("Invalid leave request");
-    }
-  };
-
   return (
-    <EmployeeContext.Provider value={{ employee, applyLeave }}>
-      <Dashboard />
-    </EmployeeContext.Provider>
+    <div>
+      
+      <h1>React Assignment</h1>
+      <ToggleComponent />
+    </div>
   );
 }
 
