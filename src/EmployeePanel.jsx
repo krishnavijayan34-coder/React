1fd72326ab5e7986696e79c7b30eb0d381a39a71
Krishna Vijayan <krishnavijayan34@gmail.com>
2026-04-23 13:22:08 +0530

    added nested components

---
+++ src/EmployeePanel.jsx
@@ -0,0 +1,12 @@
+import LeaveDetails from "./LeaveDetails";
+
+function EmployeePanel() {
+  return (
+    <div>
+      <h3>Employee Panel</h3>
+      <LeaveDetails />
+    </div>
+  );
+}
+
+export default EmployeePanel;
