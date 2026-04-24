e41059a46895c4a10a68cb10130009fd7edae81d
Krishna Vijayan <krishnavijayan34@gmail.com>
2026-04-24 11:22:28 +0530

    with and without Usecall back

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,37 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 //import heroImg from './assets/hero.png'
 import './App.css'
+import { useState, useCallback } from "react";
 
 // import EmployeeContext from "./EmployeeContext.jsx";
 // import Dashboard from "./Dashboard.jsx";
 //import CounterRef from "./CounterRef";
 //import LeaveApply from "./LeaveApply";
-import ToggleComponent from "./ToggleComponent";
+//import ToggleComponent from "./ToggleComponent";
+import Child from "./Child.jsx";
+
 function App() {
+  const [count, setCount] = useState(0);
+
+  /*const handleClick = () => {*/
+  const handleClick = useCallback(() => {
+    console.log("Clicked");
+  },[]);
+
   return (
     <div>
-      
-      <h1>React Assignment</h1>
-      <ToggleComponent />
+     <h2>Without useCallback</h2>
+     <h2>With useCallback</h2>
+
+      <button onClick={() => setCount(count + 1)}>
+        Increase Count
+      </button>
+
+      <p>Count: {count}</p>
+
+      <Child onClick={handleClick} />
     </div>
   );
 }
