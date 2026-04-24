d9c90e4ce38ac65b2aacfc30b2cd622d0a0b0d87
Krishna Vijayan <krishnavijayan34@gmail.com>
2026-04-24 13:20:55 +0530

    context provider

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,20 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 //import heroImg from './assets/hero.png'
-import './App.css'
+//import './App.css'
 import { useState, useCallback } from "react";
 
-// import EmployeeContext from "./EmployeeContext.jsx";
-// import Dashboard from "./Dashboard.jsx";
-//import CounterRef from "./CounterRef";
-//import LeaveApply from "./LeaveApply";
-//import ToggleComponent from "./ToggleComponent";
-import Child from "./Child.jsx";
+//import Child from "./child.jsx";
+import Weather from "./Weather";
+import WeatherContext from "./WeatherContext";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  /*const handleClick = () => {*/
-  const handleClick = useCallback(() => {
-    console.log("Clicked");
-  },[]);
+  const theme = "light";
 
   return (
-    <div>
-     <h2>Without useCallback</h2>
-     <h2>With useCallback</h2>
-
-      <button onClick={() => setCount(count + 1)}>
-        Increase Count
-      </button>
-
-      <p>Count: {count}</p>
-
-      <Child onClick={handleClick} />
-    </div>
+    <WeatherContext.Provider value={theme}>
+      <Weather />
+    </WeatherContext.Provider>
   );
 }
 
