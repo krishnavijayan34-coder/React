b02021abde9f43aaeaa7c3dcfec91670e29ac567
Krishna Vijayan <krishnavijayan34@gmail.com>
2026-04-24 16:59:29 +0530

    Reducer

---
+++ src/reducer.jsx
@@ -0,0 +1,11 @@
+export function reducer(state, action) {
+  if (action.type === "increment") {
+    return { count: state.count + 1 };
+  }
+
+  if (action.type === "decrement") {
+    return { count: state.count - 1 };
+  }
+
+  return state;
+}
