f3ec40c2b9f558960c653105a0bb5596f71d01fc
Krishna Vijayan <krishnavijayan34@gmail.com>
2026-04-27 13:03:08 +0530

    UseMemo & useCallback

--- App.jsx
+++ App.jsx
@@ -1,27 +1,37 @@
-import { useState } from "react";
-import DoubleNumber from "./DoubleNumber";
+import { useState,useMemo,useCallback, use } from "react";
+import ProductList from "./ProductListNew";
+const products = [
+  { id: 1, name: "Laptop", price: 50000 },
+  { id: 2, name: "Phone", price: 20000 },
+  { id: 3, name: "Tablet", price: 30000 },
+  { id: 4, name: "Headphones", price: 2000 }
+];
+function App(){
+   const[search,setSearch]=useState("");
+   const[cart,SetCart]=useState([]);
+   const filteredProducts=useMemo(()=>{
+    console.log("Searching....");
+    return products.filter((products)=>
+      products.name.toLowerCase().includes(search.toLowerCase())
+    );
+   },[search]);
 
-function App() {
-  const [count, setCount] = useState(0);
-  const [number, setNumber] = useState(5);
+   const addToCart=useCallback((products)=>{
+    SetCart((prev)=>[...prev,products]);
+   },[]);
 
-  return (
-    <div style={{ textAlign: "center" }}>
-      <h2>Simple useMemo Example</h2>
-
-      <DoubleNumber number={number} />
-
-      <button onClick={() => setCount(count + 1)}>
-        Count: {count}
-      </button>
-
-      <br /><br />
-
-      <button onClick={() => setNumber(number + 1)}>
-        Change Number ({number})
-      </button>
+   return(
+    <div>
+      <h2>Product Search</h2>
+      <input
+      type="text"
+      placeholder="Search Product"
+      value={search}
+      onChange={(e)=>setSearch(e.target.value)}
+      />
+      <ProductList products={filteredProducts} addToCart={addToCart}/>
+      <h3>Cart items;{cart.length}</h3>
     </div>
-  );
+   );
 }
-
 export default App;
