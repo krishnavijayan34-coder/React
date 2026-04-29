5a3bdfe049e7735ba5182964d5f6ce43e48c4cbd
Krishna Vijayan <krishnavijayan34@gmail.com>
2026-04-29 11:35:46 +0530

    Search Params

--- Routercomponents/User.jsx
+++ Routercomponents/User.jsx
@@ -1,8 +1,29 @@
-import { Link,Outlet } from "react-router-dom";
-function User(){
-    return (
+import { Link, Outlet, useSearchParams } from "react-router-dom";
+
+function User() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const filter = searchParams.get("filter");
+
+  const handleActiveUsers = () => {
+    setSearchParams({ filter: "active" });
+  };
+
+  const handleReset = () => {
+    setSearchParams({});
+  };
+
+  return (
     <div>
-    <h1>User List</h1>
+      <h1>User List</h1>
+
+      {/* Buttons */}
+      <button onClick={handleActiveUsers}>Active Users</button>
+      <button onClick={handleReset}>Reset Filters</button>
+
+      {/* Message based on filter */}
+      {!filter && <p>Showing all users</p>}
+      {filter === "active" && <p>Showing active users</p>}
 
       <Link to="1">User 1</Link>{" | "}
       <Link to="2">User 2</Link>{" | "}
@@ -12,4 +33,5 @@ function User(){
     </div>
   );
 }
+
 export default User;
