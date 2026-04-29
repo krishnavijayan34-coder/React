59cf281c72712d3c56e9becf3be6b9c6d88469a1
Krishna Vijayan <krishnavijayan34@gmail.com>
2026-04-29 15:56:02 +0530

    authentication app

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,43 @@ import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 
 import Home from "../Routercomponents/Home";
+import Profile from "../Routercomponents/Profile";
+import AuthProvider from "../Routercomponents/Auth";
+import Login from "../Routercomponents/Login";
+import ProtectedRoute from "../Routercomponents/ProtectedRoute";
 
 // Lazy Loading
 const About = lazy(() => import("../Routercomponents/About"));
 
 function App() {
   return (
-    <BrowserRouter>
-      <nav>
-        <NavLink to="/">Home</NavLink>{" | "}
-        <NavLink to="/about">About</NavLink>
-      </nav>
+    <AuthProvider>
+      <BrowserRouter>
+        <nav>
+          <NavLink to="/">Home</NavLink>{" | "}
+          <NavLink to="/about">About</NavLink>{" | "}
+          <NavLink to="/profile">Profile</NavLink>
+        </nav>
 
-      <Suspense fallback={<h2>Loading...</h2>}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </Suspense>
-    </BrowserRouter>
+        <Suspense fallback={<h2>Loading...</h2>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
+
+            
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
