9f34c2747261787ee593efcc21e5cc6eadd41426
Krishna Vijayan <krishnavijayan34@gmail.com>
2026-04-29 13:09:11 +0530

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,25 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 
-import Home from "./Routercomponents/Home";
-import About from "./Routercomponents/About";
-import OrderSummary from "./Routercomponents/OrderSummary";
-import OrderPage from "./Routercomponents/OrderPage";
+import Home from "../Routercomponents/Home";
 
-import Product from "../Routercomponents/Products";
-import FeaturedProduct from "../Routercomponents/FeaturedProduct";
-import New from "../Routercomponents/New";
-import User from "../Routercomponents/User";
-import UserDetails from "../Routercomponents/UserDetails";
+// Lazy Loading
+const About = lazy(() => import("../Routercomponents/About"));
 
 function App() {
   return (
     <BrowserRouter>
       <nav>
         <NavLink to="/">Home</NavLink>{" | "}
-        <NavLink to="/about">About</NavLink>{" | "}
-        <NavLink to="/products">Products</NavLink>{" | "}
-        <NavLink to="/user">User</NavLink>
+        <NavLink to="/about">About</NavLink>
       </nav>
 
-      <Routes>
-        
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/order" element={<OrderPage />} />
-        <Route path="/summary" element={<OrderSummary />} />
-
-        
-        <Route path="/products" element={<Product />}>
-          <Route index element={<FeaturedProduct />} />
-          <Route path="features" element={<FeaturedProduct />} />
-          <Route path="new" element={<New />} />
-        </Route>
-
-        
-        <Route path="/user" element={<User />}>
-        <Route path=":id" element={<UserDetails />} />
-        </Route>
-
-      </Routes>
+      <Suspense fallback={<h2>Loading...</h2>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
